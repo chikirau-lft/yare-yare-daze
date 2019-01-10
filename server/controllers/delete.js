2c9e75022fa8e7e6a1f983567d6267ed393ff580
D Litvinchuk <d.litvinchuk@dvsts.com>
2019-01-10 21:38:57 +0200

    create bulk DELETE method

--- server/controllers/delete.js
+++ server/controllers/delete.js
@@ -2,6 +2,7 @@
 
 const mongoose = require('mongoose');
 const express = require('express');
+const _ = require('lodash');
 const { ObjectId } = require('mongodb');
 
 const router = express.Router();
@@ -9,7 +10,10 @@ const router = express.Router();
 const { CommonSchema } = require('../models/common.js');
 const { ClientErrors } = require('../utils/errors.js');
 
-router.delete(`/${process.env.APP_PREFIX}/${process.env.MONGO_DATABASE}/:collection/:_id`, async(req, res) => {
+router.delete(`/${process.env.APP_PREFIX}/${process.env.MONGO_DATABASE}/:collection/:_id`, async(req, res, next) => {
+    if (req.params._id === '*')
+        return next('route');
+
     try {
         const _id = req.params._id;
 
@@ -23,6 +27,29 @@ router.delete(`/${process.env.APP_PREFIX}/${process.env.MONGO_DATABASE}/:collect
             return res.status(404).send();
  
         return res.status(200).send(document);
+    } catch(e) {
+        return res.status(400).send({
+            statusCode: 400,
+            ERROR: e.message
+        });
+    }
+});
+
+// Bulk DELETE
+router.delete(`/${process.env.APP_PREFIX}/${process.env.MONGO_DATABASE}/:collection/*`, async(req, res) => {
+    try {
+        const filter = req.query.filter !== undefined 
+            ? JSON.parse(_.replace(req.query.filter, new RegExp("\'","g"), "\"")) : '';
+
+        const collection = mongoose.model(req.params.collection, CommonSchema);
+        const documents = await collection.deleteMany(filter);
+
+        return res.status(200).send({
+            inserted: 0,
+            deleted: documents.n,
+            modified: 0,
+            matched: 0
+        });
     } catch(e) {
         return res.status(400).send({
             statusCode: 400,
