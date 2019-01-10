cff13fd61db32c2b6091124d1bd959528be29c46
D Litvinchuk <d.litvinchuk@dvsts.com>
2019-01-10 21:52:16 +0200

    create tests for Bulk DELETE

--- server/tests/controllers/delete.test.js
+++ server/tests/controllers/delete.test.js
@@ -56,3 +56,48 @@ describe(`DELETE ${process.env.APP_PREFIX}/${process.env.MONGO_DATABASE}/:collec
             .end(done);
     });
 });
+
+describe(`DELETE ${process.env.APP_PREFIX}/${process.env.MONGO_DATABASE}/:collection/*?filter=...`, () => {
+
+    beforeEach(async() => {
+        let collection = mongoose.model(testCollection, CommonSchema);
+        await collection.deleteMany({});
+        await collection.insertMany(items);
+    });
+
+    it('should delete multiple documents specified by filter param', done => {
+        request(app)
+            .delete(`/${process.env.APP_PREFIX}/${process.env.MONGO_DATABASE}/${testCollection}/*?filter={"TS": ${items[0].TS}}`)
+            .expect(200)
+            .expect(res => {
+                expect(res.body).toEqual({
+                    inserted: 0,
+                    deleted: items.filter(element => element.TS === items[0].TS).length,
+                    modified: 0,
+                    matched: 0
+                });
+            })
+            .end(async(err, res) => {
+                if (err)
+                    return done(err);
+
+                const collection = mongoose.model(testCollection, CommonSchema);
+                const documents = await collection.find({ TS: items[0] });
+
+                expect(documents).toEqual([]);
+                done();
+            });
+    });
+
+    it('should return 400 status if invalid filter obj is spesified', done => {
+        request(app)
+            .delete(`/${process.env.APP_PREFIX}/${process.env.MONGO_DATABASE}/${testCollection}/*?filter="TS": ${items[0].TS}}`)
+            .expect(400)
+            .expect(res => {
+                expect(res.body).toMatchObject({
+                    statusCode: 400
+                })
+            })
+            .end(done);
+    });
+});
