21ff2ac9e78172c60e25c58176b07da01f416bbf
SonyaMarmeladova <7exalted7@gmail.com>
2019-06-04 11:38:49 +0300

    define port as constant

--- app.js
+++ app.js
@@ -18,16 +18,17 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use(logger);
-// app.use(require('./server/controllers/patch.js'));
+app.use(require('./server/controllers/patch.js'));
 app.use(require('./server/controllers/get.js'));
-// app.use(require('./server/controllers/put.js'));
-// app.use(require('./server/controllers/post.js'));
-// app.use(require('./server/controllers/delete.js'));
+app.use(require('./server/controllers/put.js'));
+app.use(require('./server/controllers/post.js'));
+app.use(require('./server/controllers/delete.js'));
 
 const httpServer = http.createServer(app);
-httpServer.listen(process.env.PORT || 3000, async () => {
+const port = process.env.PORT || 5000;
+httpServer.listen(port, async () => {
     const time = moment().format('MMMM Do YYYY, h:mm:ss a');
-    const message = `Server is up. HTTP connections is listened on port ${process.env.PORT || 3000}, date - ${time}\n`;
+    const message = `Server is up. HTTP connections is listened on port ${port}, date - ${time}\n`;
     
     await appendFile('logs.txt', message);
 });
