9444a1d37010ac5bde2cf3ffb18f14fcfd733e57
SonyaMarmeladova <7exalted7@gmail.com>
2019-07-24 00:01:06 +0300

    apply new variable

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const moment = require('moment');
 const cors = require('cors');
+const _ = require('lodash');
 
 const appendFile = util.promisify(fs.appendFile);
+const stubMidlleware = async (req, res, next) => next();
 const { logger } = require('./server/middleware/logs.js');
 const { find } = require('./server/utils/utils.js');
 
@@ -19,11 +21,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use(logger);
-app.use(find(process.env.METHODS.split(',').map(e => e.trim()), 'GET') !== undefined ? require('./server/controllers/get.js') : async (req, res, next) => next());
-app.use(find(process.env.METHODS.split(',').map(e => e.trim()), 'POST') !== undefined ? require('./server/controllers/post.js') : async (req, res, next) => next());
-app.use(find(process.env.METHODS.split(',').map(e => e.trim()), 'PUT') !== undefined ? require('./server/controllers/put.js') : async (req, res, next) => next());
-app.use(find(process.env.METHODS.split(',').map(e => e.trim()), 'PATCH') !== undefined ? require('./server/controllers/patch.js') : async (req, res, next) => next());
-app.use(find(process.env.METHODS.split(',').map(e => e.trim()), 'DELETE') !== undefined ? require('./server/controllers/delete.js') : async (req, res, next) => next());
+app.use(find(process.env.METHODS.split(',').map(_.trim), 'GET') !== undefined ? require('./server/controllers/get.js') : stubMidlleware);
+app.use(find(process.env.METHODS.split(',').map(_.trim), 'POST') !== undefined ? require('./server/controllers/post.js') : stubMidlleware);
+app.use(find(process.env.METHODS.split(',').map(_.trim), 'PUT') !== undefined ? require('./server/controllers/put.js') : stubMidlleware);
+app.use(find(process.env.METHODS.split(',').map(_.trim), 'PATCH') !== undefined ? require('./server/controllers/patch.js') : stubMidlleware);
+app.use(find(process.env.METHODS.split(',').map(_.trim), 'DELETE') !== undefined ? require('./server/controllers/delete.js') : stubMidlleware);
 
 const httpServer = http.createServer(app);
 const port = process.env.PORT || 5000;
@@ -32,9 +34,6 @@ httpServer.listen(port, async () => {
     const message = `Server is up.HTTP connections is listened on port ${port}, date - ${time}\n`;
     
     await appendFile('logs.txt', message);
-
-    console.log(find(process.env.METHODS.split(',').map(e => e.trim()), 'POST'));
-    console.log(process.env.METHODS.split(',').map(e => e.trim()));
 });
 
 module.exports = {
