ca9380cb69e1dd47aedd2b6d01b86d5d1ee21cef
Danil Litvinchuk <d.litvinchuk@dvsts.com>
2019-12-22 14:46:08 +0200

    create one more middleware

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const http = require('http');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const moment = require('moment');
 const cors = require('cors');
-const _ = require('lodash');
 
 const { logger } = require('./server/middleware/logs.js');
 const { find } = require('./server/utils/utils.js');
@@ -17,32 +15,34 @@ const { httpMethods } = require('./server/constants/http.js');
 const { errorResponse } = require('./server/utils/errors.js');
 
 const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-app.use(logger);
-
-(async () => {
-    const methodsArr = await httpMethods();
-    app.use(find(methodsArr, 'GET') !== undefined ? require('./server/controllers/get.js') : middleware);
-    app.use(find(methodsArr, 'POST') !== undefined ? require('./server/controllers/post.js') : middleware);
-    app.use(find(methodsArr, 'PUT') !== undefined ? require('./server/controllers/put.js') : middleware);
-    app.use(find(methodsArr, 'PATCH') !== undefined ? require('./server/controllers/patch.js') : middleware);
-    app.use(find(methodsArr, 'DELETE') !== undefined ? require('./server/controllers/delete.js') : middleware);
-    app.use((req, res, next) => {
+app
+    .use(bodyParser.json())
+    .use(cors())
+    .use(async (req, res, next) => {
+        const allowedMethods = await httpMethods();
+        if (!allowedMethods.includes(req.method)) 
+            return errorResponse(res, 405, 'Method Not Allowed.');
+        next();
+    })
+    .use(require('./server/controllers/get.js'))
+    .use(require('./server/controllers/post.js'))
+    .use(require('./server/controllers/put.js'))
+    .use(require('./server/controllers/patch.js'))
+    .use(require('./server/controllers/delete.js'))
+    .use((req, res, next) => {
         const err = new Error('Requested resourse not found.');
         err.status = 404;
         next(err);
     })
-    app.use(function(err, req, res, next) {
+    .use(function(err, req, res, next) {
         return errorResponse(res, 400, err.message);
     });
-})();
+
 
 const httpServer = http.createServer(app);
 const port = process.env.PORT || 5000;
 httpServer.listen(port, async () => {
-    const time = moment().format('MMMM Do YYYY, h:mm:ss a');
-    const message = `Server is up.HTTP connections is listened on port ${port}, date - ${time}\n`;
+    const message = `Server is up. HTTP connections is listened on port ${port}\n`;
     console.log(message);
 });
 
